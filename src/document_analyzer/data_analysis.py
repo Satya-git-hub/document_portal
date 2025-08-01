9de75a6617db3b08a29ae5f925d12d991ef89afb
satyabrata.behera <sb22844@gmail.com>
2025-08-01 13:05:53 +0530

    testing of model data analyze done

--- src/document_analyzer/data_analysis.py
+++ src/document_analyzer/data_analysis.py
@@ -1,95 +1,59 @@
 import os
-import fitz
-import uuid
-from datetime import datetime
+import sys
+from utils.model_loader import ModelLoader
 from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
+from model.models import *
+from langchain_core.output_parsers import JsonOutputParser
+from langchain.output_parsers import OutputFixingParser
+from prompt.prompt_library import *
 
-class DocumentHandler:
+class DocumentAnalyzer:
     """
-    Handles PDF saving and reading operations.
+    Analyzes documents using a pre-trained model.
     Automatically logs all actions and supports session-based organization.
     """
-    def __init__(self,data_dir=None,session_id=None):
+    def __init__(self):
+        self.log = CustomLogger().get_logger(__name__)
         try:
-            self.log=CustomLogger().get_logger(__name__)
-            self.data_dir = data_dir or os.getenv(
-                "DATA_STORAGE_PATH",
-                os.path.join(os.getcwd(), "data", "document_analysis")
-            )
-            self.session_id = session_id or f"session_{datetime.utcnow().strftime('%Y%m%d_%H%M%S')}_{uuid.uuid4().hex[:8]}"
+            self.loader=ModelLoader()
+            self.llm=self.loader.load_llm()
             
-            # Create base session directory
-            self.session_path = os.path.join(self.data_dir, self.session_id)
+            # Prepare parsers
+            self.parser = JsonOutputParser(pydantic_object=Metadata)
+            self.fixing_parser = OutputFixingParser.from_llm(parser=self.parser, llm=self.llm)
             
-            os.makedirs(self.session_path, exist_ok=True)
+            self.prompt = prompt
+            
+            self.log.info("DocumentAnalyzer initialized successfully")
+            
+            
+        except Exception as e:
+            self.log.error(f"Error initializing DocumentAnalyzer: {e}")
+            raise DocumentPortalException("Error in DocumentAnalyzer initialization", sys)
+        
+        
+    
+    def analyze_document(self, document_text:str)-> dict:
+        """
+        Analyze a document's text and extract structured metadata & summary.
+        """
+        try:
+            chain = self.prompt | self.llm | self.fixing_parser
+            
+            self.log.info("Meta-data analysis chain initialized")
 
-            self.log.info("PDFHandler initialized", session_id=self.session_id, session_path=self.session_path)
+            response = chain.invoke({
+                "format_instructions": self.parser.get_format_instructions(),
+                "document_text": document_text
+            })
+
+            self.log.info("Metadata extraction successful", keys=list(response.keys()))
+            
+            return response
 
         except Exception as e:
-            self.log.error(f"Error initializing DocumentHandler: {e}")
-            raise DocumentPortalException("Error initializing DocumentHandler", e) from e
+            self.log.error("Metadata analysis failed", error=str(e))
+            raise DocumentPortalException("Metadata extraction failed") from e
         
-
-    def save_pdf(self,uploaded_file):
-        try:
-            filename = os.path.basename(uploaded_file.name)
-            
-            if not filename.lower().endswith(".pdf"):
-                raise DocumentPortalException("Invalid file type. Only PDFs are allowed.")
-
-            save_path = os.path.join(self.session_path, filename)
-            
-            with open(save_path, "wb") as f:
-                f.write(uploaded_file.getbuffer())
-
-            self.log.info("PDF saved successfully", file=filename, save_path=save_path, session_id=self.session_id)
-            
-            return save_path
-        
-        except Exception as e:
-            self.log.error(f"Error saving PDF: {e}")
-            raise DocumentPortalException("Error saving PDF", e) from e
-
-    def read_pdf(self, pdf_path:str)->str:
-        try:
-            text_chunks = []
-            with fitz.open(pdf_path) as doc:
-                for page_num, page in enumerate(doc, start=1):
-                    text_chunks.append(f"\n--- Page {page_num} ---\n{page.get_text()}")
-            text = "\n".join(text_chunks)
-
-            self.log.info("PDF read successfully", pdf_path=pdf_path, session_id=self.session_id, pages=len(text_chunks))
-            return text
-        except Exception as e:
-            self.log.error(f"Error reading PDF: {e}")
-            raise DocumentPortalException("Error reading PDF", e) from e
-    
-if __name__ == "__main__":
-    from pathlib import Path
-    from io import BytesIO
-    
-    pdf_path=r"C:\Users\bsaty\Projects\document_portal\data\document_analysis\sample.pdf"
-    class DummnyFile:
-        def __init__(self,file_path):
-            self.name = Path(file_path).name
-            self._file_path = file_path
-        def getbuffer(self):
-            return open(self._file_path, "rb").read()
-        
-    dummy_pdf = DummnyFile(pdf_path)
-    
-    handler = DocumentHandler()
-    
-    try:
-        saved_path=handler.save_pdf(dummy_pdf)
-        print(saved_path)
-        
-        content=handler.read_pdf(saved_path)
-        print("PDF Content:")
-        print(content[:500])  # Print first 500 characters of the PDF content
-        
-    except Exception as e:
-        print(f"Error: {e}")
-    
     
